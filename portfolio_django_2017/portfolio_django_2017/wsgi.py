6aa5b4ee9fa2627857e3a3f293ad2ca13a37fb21
ivangolubykh <ivan9@allworld.xyz>
2017-04-06 09:01:38 +0300

    Произведены начальные настройки проекта. Произведена инсталляция первого приложения. Произведена миграция БД (для работы панели отладки). Произведена проверка всех файлов на соответствие pep8.

--- portfolio_django_2017/portfolio_django_2017/wsgi.py
+++ portfolio_django_2017/portfolio_django_2017/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio_django_2017.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "portfolio_django_2017.settings")
 
 application = get_wsgi_application()
