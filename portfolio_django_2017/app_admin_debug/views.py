30f3d2aa8b97847066bc4d1335edb11bbff7ffeb
ivangolubykh <ivan9@allworld.xyz>
2017-04-08 18:03:54 +0300

    Создано приложение временной Админики. Приложение временной админки настроено на работу только в режиме отладки. Создана заготовка для главной страницы временной админки.

--- portfolio_django_2017/app_admin_debug/views.py
+++ portfolio_django_2017/app_admin_debug/views.py
@@ -1,13 +1,12 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
-from .models import MainHeader1Text, MainHeader2Text, MainHeader3Text,\
+from app_main.models import MainHeader1Text, MainHeader2Text, MainHeader3Text,\
     MainHeader4Text, MainText
 
 
-# Главная страница:
-class MainListView(ListView):
-    # model = MainText
-    queryset = MainHeader1Text.objects.order_by('ordinal').\
+# Главная страница админки:
+class AdminListView(ListView):
+    queryset = MainHeader1Text.objects.order_by('ordinal'). \
         prefetch_related('mainheader2text_set',
                          'mainheader2text_set__mainheader3text_set',
                          'mainheader2text_set__mainheader3text_set__'
