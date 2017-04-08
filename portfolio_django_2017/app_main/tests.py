e20a0a83e8e0edab3e83c31f66c6c0bc3c7e92e0
ivangolubykh <ivan9@allworld.xyz>
2017-04-08 17:41:14 +0300

    В tests.py добавлен тест главной страницы.

--- portfolio_django_2017/app_main/tests.py
+++ portfolio_django_2017/app_main/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class TestMainPage(TestCase):
+
+    def test_main_page(self):
+        """
+        """
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
