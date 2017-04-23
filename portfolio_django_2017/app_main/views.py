aca95df0ea4ca5fcbe6ed021c02285d0bc1e08c9
ivangolubykh <ivan9@allworld.xyz>
2017-04-23 08:42:40 +0300

    Настроено получение по api файла с данными реальной погоды.

--- portfolio_django_2017/app_main/views.py
+++ portfolio_django_2017/app_main/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import MainHeader1Text
+import http.client
 
 
 # Главная страница:
@@ -16,6 +17,19 @@ class MainListView(ListView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+        try:
+            weather_server = 'api.wunderground.com'
+            weather_page = '/api/d3de2d8a8d6e227e/geolookup/conditions/' \
+                           'forecast/lang:RU/q/Russia/Saint%20Petersburg.json'
+            conn = http.client.HTTPSConnection(weather_server, timeout=25)
+            conn.request("GET", weather_page)
+            obj_conn = conn.getresponse()
+            text = obj_conn.read()
+            conn.close()
+        except Exception as err:
+            text = 'Получить погоду не удалось: '  # + err
+
         context = super().get_context_data(**kwargs)
-        context['weather_text'] = 'Тут будет реальная погода!!!'
+        context['weather_text'] = 'Тут будет реальная погода!!! - '\
+                                  + str(text)
         return context
