196147739c995f3e54ee9eba9c913b81d774e31c
ivangolubykh <ivan9@allworld.xyz>
2017-05-01 14:26:31 +0300

    Созданы хлебные крошки и создана заготовка для страницы примеров работ.

--- portfolio_django_2017/app_main/views.py
+++ portfolio_django_2017/app_main/views.py
@@ -6,11 +6,33 @@ from django.http import JsonResponse
 from portfolio_django_2017.settings import STATIC_URL
 from django.db import transaction
 from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+
+def crumbs(object):
+    ''' Функция генерации хлебных крошек. Для её работы необходимо в каждой
+    функции/классе иметь 3 переменных:
+        crumbs_page_name = < Текстовое название ссылки на эту страницу >
+        crumbs_page_urlname = < Имя страницы в UrlConf (для reverse) >
+        crumbs_up = < Ссылка на функцию/класс родительской страницы >
+    Для запуска функции в качестве параметра object надо передать ссылку на
+    текущую функцию или класс, из которой вызваны хлебные крошки, например:
+     crumbs(__class__)
+    '''
+    html_str = ''
+    if object.crumbs_up:
+        html_str += crumbs(object.crumbs_up) + ' &rarr; '
+    html_str += '<a href="{}">{}</a>'.\
+        format(reverse(object.crumbs_page_urlname), object.crumbs_page_name)
+    return html_str
 
 
 # Главная страница:
 class MainListView(ListView):
     # model = MainText
+    crumbs_page_name = 'Главная'
+    crumbs_page_urlname = 'main'
+    crumbs_up = False
     queryset = MainHeader1Text.objects.order_by('ordinal').\
         prefetch_related('mainheader2text_set',
                          'mainheader2text_set__mainheader3text_set',
@@ -30,6 +52,7 @@ class MainListView(ListView):
         return context
 
 
+# Генерация Json с погодой для аякса на главной странице.
 def weather_json(request):
     # Буду обновлять БД не чаще 1 раза в 15 минут (900 секунд):
     TIME_DELTA_TO_UPDATE_WEATHER = 900
@@ -85,3 +108,29 @@ def weather_json(request):
     content['weather_text'] = 'Данные взяты с сайта allworld.xyz и' \
                               ' предназначены только для его посетителей.'
     return JsonResponse(content)
+
+
+# Страница Примеры Работ:
+class ExamplesWorkListView(ListView):
+    crumbs_page_name = 'Примеры работ'
+    crumbs_page_urlname = 'examples_work'
+    crumbs_up = MainListView
+    # model = MainText
+    queryset = MainHeader1Text.objects.order_by('ordinal').\
+        prefetch_related('mainheader2text_set',
+                         'mainheader2text_set__mainheader3text_set',
+                         'mainheader2text_set__mainheader3text_set__'
+                         'mainheader4text_set',
+                         'mainheader2text_set__mainheader3text_set__'
+                         'mainheader4text_set__maintext_set')
+    template_name = 'examples_work.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # создаю сессию для подрузки данных Ajax-ом только для своих
+        # посетителей:
+        if 'session_exist' not in self.request.session or\
+                not self.request.session['session_exist']:
+            self.request.session['session_exist'] = True
+        context['crumbs'] = crumbs(__class__)
+        return context
