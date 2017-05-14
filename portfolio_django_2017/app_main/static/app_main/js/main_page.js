cfa2757d44be0f8e5a96626a07ecc1176e63babf
ivangolubykh <ivan9@allworld.xyz>
2017-05-14 11:03:04 +0300

    Блок показа погоды в СПб вынесен в отдельный шаблон. Ajax-скрипт показа погоды вынесен в отдельный файл. Всё для того, чтобы показывать погоду на разных страницах сайта.

--- portfolio_django_2017/app_main/static/app_main/js/main_page.js
+++ portfolio_django_2017/app_main/static/app_main/js/main_page.js
@@ -1,19 +1,5 @@
 'use strict';
 
-function get_weather() {
-    $.ajax({
-            type: "GET",
-            url: get_weather_url,
-            data: '',
-            dataType: "json",
-            success: function(data){
-                document.getElementById('weather_img').src = data.weather_image;
-                document.getElementById('weather_temperature').innerHTML = data.weather_temperature + '&deg;C,';
-                document.getElementById('weather_text').innerHTML = data.weather_text;
-            }
-    });
-}
-
 function arcana() {
     /* Загадки для посетителей */
     function arcana_template(arcana_data_element) {
---
+++ portfolio_django_2017/general_static/general/js/weather.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function get_weather() {
+    $.ajax({
+            type: "GET",
+            url: get_weather_url,
+            data: '',
+            dataType: "json",
+            success: function(data){
+                document.getElementById('weather_img').src = data.weather_image;
+                document.getElementById('weather_temperature').innerHTML = data.weather_temperature + '&deg;C,';
+                document.getElementById('weather_text').innerHTML = data.weather_text;
+            }
+    });
+}
