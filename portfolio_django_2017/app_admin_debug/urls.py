ca7d325b2907ad07727b90688d9dda0eda67f28b
ivangolubykh <ivan9@allworld.xyz>
2017-05-15 09:09:09 +0300

    Изменена структура БД для страницы примеров работ на Питоне.

--- portfolio_django_2017/app_admin_debug/urls.py
+++ portfolio_django_2017/app_admin_debug/urls.py
@@ -18,14 +18,8 @@ from .views import AdminListView, H1Create, H2Create, H3Create, H4Create, \
     TextCreate, H1Delete, H2Delete, H3Delete, H4Delete, TextDelete, H1Update,\
     H2Update, H3Update, H4Update, TextUpdate,\
     AdminExamplesPythonListView,\
-    AdminExamplesPythonH1Create, AdminExamplesPythonH2Create,\
-    AdminExamplesPythonH3Create, AdminExamplesPythonH4Create,\
-    AdminExamplesPythonTextCreate, AdminExamplesPythonH1Delete,\
-    AdminExamplesPythonH2Delete, AdminExamplesPythonH3Delete,\
-    AdminExamplesPythonH4Delete, AdminExamplesPythonTextDelete,\
-    AdminExamplesPythonH1Update, AdminExamplesPythonH2Update,\
-    AdminExamplesPythonH3Update, AdminExamplesPythonH4Update,\
-    AdminExamplesPythonTextUpdate,\
+    AdminExamplesPythonH1Create, AdminExamplesPythonH1Delete,\
+    AdminExamplesPythonH1Update,\
     AdminExamplesJsListView, \
     AdminExamplesJsH1Create, AdminExamplesJsH2Create, \
     AdminExamplesJsH3Create, AdminExamplesJsH4Create, \
@@ -102,48 +96,12 @@ urlpatterns += [
     url(r'admin_examples_work_python/add_main_h1/$',
         AdminExamplesPythonH1Create.as_view(),
         name='admin_examples_work_python-add_h1'),
-    url(r'admin_examples_work_python/add_main_h2/$',
-        AdminExamplesPythonH2Create.as_view(),
-        name='admin_examples_work_python-add_h2'),
-    url(r'admin_examples_work_python/add_main_h3/$',
-        AdminExamplesPythonH3Create.as_view(),
-        name='admin_examples_work_python-add_h3'),
-    url(r'admin_examples_work_python/add_main_h4/$',
-        AdminExamplesPythonH4Create.as_view(),
-        name='admin_examples_work_python-add_h4'),
-    url(r'admin_examples_work_python/add_main_text/$',
-        AdminExamplesPythonTextCreate.as_view(),
-        name='admin_examples_work_python-add_text'),
     url(r'admin_examples_work_python/del_main_h1/(?P<pk>[\w]+)/$',
         AdminExamplesPythonH1Delete.as_view(),
         name='admin_examples_work_python-del_h1'),
-    url(r'admin_examples_work_python/del_main_h2/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonH2Delete.as_view(),
-        name='admin_examples_work_python-del_h2'),
-    url(r'admin_examples_work_python/del_main_h3/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonH3Delete.as_view(),
-        name='admin_examples_work_python-del_h3'),
-    url(r'admin_examples_work_python/del_main_h4/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonH4Delete.as_view(),
-        name='admin_examples_work_python-del_h4'),
-    url(r'admin_examples_work_python/del_main_text/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonTextDelete.as_view(),
-        name='admin_examples_work_python-del_text'),
     url(r'admin_examples_work_python/update_main_h1/(?P<pk>[\w]+)/$',
         AdminExamplesPythonH1Update.as_view(),
         name='admin_examples_work_python-update_h1'),
-    url(r'admin_examples_work_python/update_main_h2/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonH2Update.as_view(),
-        name='admin_examples_work_python-update_h2'),
-    url(r'admin_examples_work_python/update_main_h3/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonH3Update.as_view(),
-        name='admin_examples_work_python-update_h3'),
-    url(r'admin_examples_work_python/update_main_h4/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonH4Update.as_view(),
-        name='admin_examples_work_python-update_h4'),
-    url(r'admin_examples_work_python/update_main_text/(?P<pk>[\w]+)/$',
-        AdminExamplesPythonTextUpdate.as_view(),
-        name='admin_examples_work_python-update_text'),
 ]
 
 # Блок страниц админки для страницы примеров работ на JavaScript:
