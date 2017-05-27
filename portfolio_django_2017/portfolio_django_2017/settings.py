7682faf5a375e331a7cf5dfca10b5b3e182d2195
ivangolubykh <ivan9@allworld.xyz>
2017-05-27 19:22:04 +0300

    Изменён секретный ключ Django для облегчения его автозамены на боевом сервере.

--- portfolio_django_2017/portfolio_django_2017/settings.py
+++ portfolio_django_2017/portfolio_django_2017/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rj%i^of$m!k(-f)j26#6*51v4^su#21ta1rw(lgmtp8=0-pr#+'
+SECRET_KEY = 'secretkey123secretkey123secretkey123secretkey123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
